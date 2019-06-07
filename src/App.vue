2b2f45986c1356a63a7b70441b79654727645f9e
James Wu <therealjameswu@gmail.com>
2019-06-07 14:57:06 -0400

    Initial commit with base navigation bar

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app>
+    <NavigationBar/>
+    <v-content></v-content>
+  </v-app>
+</template>
+
+<script>
+import NavigationBar from "@/components/NavigationBar";
+
+export default {
+  name: "App",
+  components: {
+    NavigationBar
+  },
+  data() {
+    return {
+      //
+    };
+  }
+};
+</script>
