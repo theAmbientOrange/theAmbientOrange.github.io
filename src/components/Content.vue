8d377d9687912977ba7bc63160be6829e2af0467
James Wu <therealjameswu@gmail.com>
2019-06-07 22:35:42 -0400

    Ref to side projects

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -12,6 +12,13 @@
 
     <div id="side-projects">
       <h1>side projects</h1>
+      <p>
+        These projects can be found on
+        <a
+          href="https://github.com/theAmbientOrange"
+          target="_blank"
+        >my Github</a>.
+      </p>
     </div>
 
     <div id="career">
