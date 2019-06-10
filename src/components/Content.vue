3329d7214b0cfafeecb11c133498c6f5e442bad5
James Wu <therealjameswu@gmail.com>
2019-06-10 19:20:47 -0400

    Update about me section

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -6,11 +6,21 @@
       <p>Hello! My name is James Wu, and thank you for visiting my website.</p>
       <p>My primary interests are in video game development, writing C++ code, and real-time systems.</p>
       <p>I received my Bachelor of Science degree in Computer Science from the University of Virginia.</p>
+      <p>I am proficient with C/C++, Java, and Python. I am familiar with Swift and Javascript.</p>
       <p>I am currently a software engineer in Small Business Card Tech at Capital One.</p>
       <p>Outside of work, I love making video games using Unreal Engine 4, attempting to make 3D models using Blender, running marathons, hiking, playing video games, and watching MIT OpenCourseware videos.</p>
     </div>
-    <SideProjects/>
     <Career/>
+    <SideProjects/>
+    <div id="contact">
+      <h1>contact</h1>
+      <p>
+        <v-icon>phone</v-icon>703-439-7279
+      </p>
+      <p>
+        <v-icon>email</v-icon>therealjameswu@gmail.com
+      </p>
+    </div>
   </div>
 </template>
 
