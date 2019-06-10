ee79d0de87dc7b64c3f3e4d648df94b3145421a1
James Wu <therealjameswu@gmail.com>
2019-06-09 20:34:19 -0400

    Move SideProjects card logic to separate component

--- src/components/SideProjects.vue
+++ src/components/SideProjects.vue
@@ -2,7 +2,7 @@
   <div id="side-projects">
     <h1>side projects</h1>
     <p>I love programming, and I find the best way to learn is by doing. So here are my random projects.</p>
-    <p>I'm experimenting with C++, Unreal Engine 4, iOS development, VueJS, Slackbots... and I've had the privilege of working on some of these with extremely talented people.</p>
+    <p>I'm experimenting with C++, Unreal Engine 4, iOS development, VueJS, Slackbots... and I've had the privilege of working on some of these projects with extremely talented people.</p>
     <p>
       These projects can be found on
       <a
@@ -13,24 +13,13 @@
     <v-card>
       <v-container fluid grid-list-md>
         <v-layout row wrap>
-          <v-flex v-for="item in items" :key="item.title" v-bind="{ [`xs${item.flex}`]: true }">
-            <v-card id="side-project-card">
-              <v-img :src="item.src" height="80%">
-                <v-container fill-height fluid pa-2>
-                  <v-layout fill-height></v-layout>
-                </v-container>
-              </v-img>
-              <v-card-actions>
-                {{item.title}}
-                <v-spacer></v-spacer>
-                <v-btn icon :href="item.githubUrl" target="_blank">
-                  <img src="../assets/github-circle.svg">
-                </v-btn>
-                <v-btn icon>
-                  <v-icon color="pink">description</v-icon>
-                </v-btn>
-              </v-card-actions>
-            </v-card>
+          <v-flex
+            v-for="item in items"
+            :key="item.title"
+            v-bind="{ [`sm${item.flex}`]: true }"
+            id="yee"
+          >
+            <SideProjectsCard :cardDetails="item"/>
           </v-flex>
         </v-layout>
       </v-container>
@@ -39,22 +28,56 @@
 </template>
 
 <script>
+import SideProjectsCard from "@/components/SideProjectsCard";
+
 export default {
   data: () => ({
     items: [
       {
+        title: "Miracle Messages",
+        description: "Greetings boi",
+        githubUrl: "https://github.com/miracle-messages/mm-android-mobile",
+        src:
+          "https://images.unsplash.com/photo-1534448177492-6d698f12a59a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80",
+        flex: 6,
+        showDescription: false
+      },
+      {
         title: "Big Tuna",
-        description: "yo",
-        githubUrl: "https://github.com/aomori-nebuta/bigtuna-ios",
-        src: "https://cdn.vuetifyjs.com/images/cards/road.jpg",
-        flex: 6
+        description: "Greetings boi",
+        githubUrl: "https://github.com/miracle-messages/mm-android-mobile",
+        src:
+          "https://raw.githubusercontent.com/theAmbientOrange/theAmbientOrange.github.io/master/src/assets/miracle-messages.png",
+        flex: 6,
+        showDescription: false
       }
     ]
-  })
+  }),
+  components: {
+    SideProjectsCard
+  }
 };
 </script>
 
 <style>
+#paul {
+  width: 100%;
+  height: 50vw;
+  position: relative;
+}
+
+#guy {
+  height: 100%;
+  border: none;
+  position: absolute;
+  bottom: 0px;
+}
+.side-project-image {
+  width: 50vw;
+  height: 50vw;
+  object-fit: contain;
+  overflow: hidden;
+}
 #side-project-card {
   border-radius: 30px;
 }
---
+++ src/components/SideProjectsCard.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-card id="side-project-card">
+    <v-container class="paul">
+      <v-layout align-center justify-center>
+        <img class="side-project-image" v-if="!cardDetails.showDescription" :src="cardDetails.src">
+        <!-- <v-container fill-height fluid pa-2>
+                  <v-layout fill-height></v-layout>
+        </v-container>-->
+        <v-textarea
+          v-else
+          class="align-center justify-center fill-height text-xs-center pa-1"
+          id="guy"
+          disabled
+          solo
+          outline
+          value="yo"
+        ></v-textarea>
+      </v-layout>
+    </v-container>
+
+    <v-card-actions>
+      {{cardDetails.title}}
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon color="pink" @click="handleOnDescriptionClick(cardDetails)">description</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon color="green">important_devices</v-icon>
+      </v-btn>
+      <v-btn icon :href="cardDetails.githubUrl" target="_blank">
+        <img src="../assets/github-circle.svg">
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    cardDetails: {
+      type: Object,
+      default: () => {}
+    }
+  },
+  methods: {
+    handleOnDescriptionClick(cardDetails) {
+      console.log("YEEEEAAAH " + cardDetails.showDescription);
+      cardDetails.showDescription = !cardDetails.showDescription;
+    }
+  }
+};
+</script>
+
+<style>
+</style>
